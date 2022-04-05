940b15fafdbc227cc319b22360811ac1cc45d5c1
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-04-05 18:28:24 +0100

    connected to mongo db

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import cors from "cors"
+
+
+/********************** Port *******************/
+const server = express()
+const PORT =  process.env.PORT || 3001
+
+
+/***************************  middleware ***********************/
+server.use(express.json())
+server.use(cors())
+/****************************  routes **************************/
+
+/************************* Error Handler ***********************/
+
+/**************************   connection ***********************/
+mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connection.on("connected",() => {
+    console.log("Successfully connected to mongo!")
+})
+
+server.listen(PORT, () => {
+console.table(listEndpoints(server))
+console.log("The server is running in the port - ", PORT)
+})
+
+server.on("error", (error) => {
+    console.log("Server has stopped due to error", error)
+})
+
