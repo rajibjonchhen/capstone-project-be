2152cd0995d849b9e60b2b3deb1528587655d5ad
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-04-06 11:35:40 +0100

    all users route protected with jwt token

--- src/services/users/users.js
+++ src/services/users/users.js
@@ -1,11 +1,24 @@
 import express from "express";
 import createHttpError from "http-errors";
+import multer from "multer";
 import { JWTAuthMW } from "../authentication/JWTAuthMW.js";
 import { authenticateUser } from "../authentication/tools.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary"
 import UserModel from "./user-schema.js"
 
-const usersRouter = express.Router()
 
+const cloudinaryAvatarUploader = multer({
+    storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "creators-space",
+        },
+    }),
+}).single("avatar")
+
+
+const usersRouter = express.Router()
 /***************************  register new user ***********************/
 .post("/signUp", async(req, res, next) => {
     try {
@@ -68,7 +81,10 @@ const usersRouter = express.Router()
 /****************************  edit my detail *************************/
 .put("/me",  JWTAuthMW, async(req, res, next) => {
     try {
-        
+        if(req.user){
+            const user = await UserModel.findByIdAndUpdate(req.user._id, req.body, {new: true})
+            res.send({user})
+        }
     } catch (error) {
         next(createHttpError(error))
     }
@@ -78,7 +94,10 @@ const usersRouter = express.Router()
 /***************************  delete my detail ************************/
 .delete("/me",  JWTAuthMW, async(req, res, next) => {
     try {
-        
+        if(req.user){
+            const user = await UserModel.findByIdAndDelete(req.user._id)
+            res.send()
+        }
     } catch (error) {
         next(createHttpError(error))
     }
@@ -86,15 +105,26 @@ const usersRouter = express.Router()
 
 
 /*****************************  add my avatar *************************/
-.post("/me/avatar", async(req, res, next) => {
+.post("/me/avatar",JWTAuthMW, cloudinaryAvatarUploader,  async(req, res, next) => {
     try {
-        
+        if(req.user){
+            const updatedUser = await UserModel.findByIdAndUpdate(req.user._id, {avatar:req.file.path}, {new:true})
+            
+        }
     } catch (error) {
         next(createHttpError(error))
     }
 })
 
 
+/*****************************  logout user *************************/
+.delete("/session", async(req, res, next) => {
+    try {
+        res.send({token : null})
+    } catch (error) {
+        next(createHttpError(error))
+    }
+})
 
 
 export default usersRouter
