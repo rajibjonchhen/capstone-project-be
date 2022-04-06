563103fd5e7473630b0720bf96eaee724fdad005
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-04-06 16:08:27 +0100

    comment schema added

--- src/services/posts/post-schema.js
+++ src/services/posts/post-schema.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
 const {model, Schema} = mongoose
+const CommentSchema = new Schema({
+    comment:{type:String},
+    commentedBy : {type:Schema.Types.ObjectId, ref: "User"},
+},{
+
+    timestamps: true
+})
+
 const PostSchema = new Schema({
     content : {type:String, required:true},
     postedBy : {type:Schema.Types.ObjectId, ref: "User"},
     likes : [{type: Schema.Types.ObjectId, ref:"User"}],
-    comments : [{
-        comment:{type:String},
-        commentedBy : {type:Schema.Types.ObjectId, ref: "User"},
-    }]
+    comments : [{type:CommentSchema}]
 },{
     timestamps: true
 })
--- src/services/posts/posts.js
+++ src/services/posts/posts.js
@@ -15,11 +15,11 @@ const postsRouter = express.Router()
 /***************************  admin only routes ************************/
 
 /***************************  get post by id route ************************/
-.get("/:id",  JWTAuthMW, adminMW, async(req, res, next) => {
+.get("/:postId",  JWTAuthMW, adminMW, async(req, res, next) => {
     try {
         if(req.user.role==="admin"){
             console.log("admin search only")
-            const post = await PostModel.findById(req.params.id)
+            const post = await PostModel.findById(req.params.postId)
             res.send({post})
         }
     } catch (error) {
@@ -28,10 +28,10 @@ const postsRouter = express.Router()
 })
 
 /***************************  edit post by id route ************************/
-.put("/:id",  JWTAuthMW, adminMW, async(req, res, next) => {
+.put("/:postId",  JWTAuthMW, adminMW, async(req, res, next) => {
     try {
         if(req.user.role==="admin"){
-            const updatedPost = await PostModel.findByIdAndUpdate(req.params.id, req.body,{new:true})
+            const updatedPost = await PostModel.findByIdAndUpdate(req.params.postId, req.body,{new:true})
             res.send({post : updatedPost})
         }
     } catch (error) {
@@ -40,10 +40,10 @@ const postsRouter = express.Router()
 })
 
 /***************************  delete Post by id route ************************/
-.delete("/:id",  JWTAuthMW, adminMW, async(req, res, next) => {
+.delete("/:postId",  JWTAuthMW, adminMW, async(req, res, next) => {
     try {
         if(req.user.role==="admin"){
-            await PostModel.findByIdAndDelete(req.params.id)
+            await PostModel.findByIdAndDelete(req.params.postId)
             res.send()
         }
     } catch (error) {
@@ -55,7 +55,7 @@ const postsRouter = express.Router()
 
 /***************************  Post routes ************************/
 
-/***************************  register new Post ***********************/
+/***************************  create new Post ***********************/
 .post("/",JWTAuthMW, async(req, res, next) => {
     try {
         const newPost = new PostModel({...req.body, postedBy: req.user._id})
@@ -96,10 +96,10 @@ const postsRouter = express.Router()
 })
 
 /*****************************  get my detail *************************/
-.get("/me/:id",  JWTAuthMW, async(req, res, next) => {
+.get("/me/:postId",  JWTAuthMW, async(req, res, next) => {
     try {
         if(req.user){
-            const post = await PostModel.findById(req.params.id)
+            const post = await PostModel.findById(req.params.postId)
             res.send({post})
         }
     } catch (error) {
@@ -109,15 +109,15 @@ const postsRouter = express.Router()
 
 
 /****************************  edit my post *************************/
-.put("/me/:id",  JWTAuthMW, async(req, res, next) => {
+.put("/me/:postId",  JWTAuthMW, async(req, res, next) => {
     try {
-        const post = await PostModel.findById(req.params.id)
+        const post = await PostModel.findById(req.params.postId)
         if(post){
             console.log("post.postedBy -", post.postedBy)
             console.log("post.postedBy.toObject", post.postedBy.toString())
             console.log("req.user._id", req.user._id)
             if(post.postedBy.toString() === req.user._id){
-                const updatedPost = await PostModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
+                const updatedPost = await PostModel.findByIdAndUpdate(req.params.postId, req.body, {new: true})
                 res.send({updatedPost})
             }else{
                 console.log("not authorised to update this post");
@@ -134,14 +134,14 @@ const postsRouter = express.Router()
 
 
 /***************************  delete my post ************************/
-.delete("/me/:id",  JWTAuthMW, async(req, res, next) => {
+.delete("/me/:postId",  JWTAuthMW, async(req, res, next) => {
     try {
         if(req.user){
-            const post = await PostModel.findById(req.params.id)
+            const post = await PostModel.findById(req.params.postId)
             if(post){
-                if(post.postedBy.toString === req.user._id){
+                if(post.postedBy.toString() === req.user._id){
 
-                    await PostModel.findByIdAndDelete(req.params.id)
+                    await PostModel.findByIdAndDelete(req.params.postId)
                     res.send()
                 }else {
                     next(createError(401, {message:"not authorised to delete this post"}))
@@ -156,4 +156,40 @@ const postsRouter = express.Router()
 })
 
 
+/***************************  comments section ************************/
+
+/***************************  comment a post ************************/
+.post("/:postId/comments",JWTAuthMW, async(req, res, next) => {
+    try {
+        const reqPost = await PostModel.findById(req.params.postId)
+        if(reqPost){
+                const newComment = {...req.body, commentedBy: req.user._id}
+                const updatedPost = await PostModel.findByIdAndUpdate(req.params.postId,{$push:{comments: newComment}}, {new:true})
+            if(updatedPost){
+
+                res.send({updatedPost})
+            } else {
+                next(createError(401, " error - could not post a comment"))
+            }
+        } else {
+            next(createError(401, "bad request could not find the required post"))
+        }
+    } catch (error) {
+        next(createError(error))
+    }
+})
+
+/***************************  get all the comments of a post ************************/
+.get("/:postId/comments",JWTAuthMW, async(req, res, next) => {
+    try {
+        const reqPost = await PostModel.findById(req.params.postId)
+        if(reqPost){
+                res.send({comments:reqPost.comments})
+        } else {
+            next(createError(401, "bad request could not find the required post"))
+        }
+    } catch (error) {
+        next(createError(error))
+    }
+})
 export default postsRouter
